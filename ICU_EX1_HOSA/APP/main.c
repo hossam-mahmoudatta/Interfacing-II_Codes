53ae69009ec67663578058fa734b5527c16add3b
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-05-13 01:51:44 +0300

    Uploading Interrupt Codes

---
+++ ICU_EX1_HOSA/APP/main.c
@@ -0,0 +1,39 @@
+/*
+ * main.c
+ *
+ *  Created on: May 2, 2023
+ *      Author: Administrator
+ */
+#include <util/delay.h>
+#include <avr/io.h>
+//#include "../ECUAL/LED_DRIVER/LED.h"
+//#include "../ECUAL/BUTTON_DRIVER/BUTTON.h"
+//#include "../ECUAL/SEVEN-SEGMENT_DRIVER/SEGMENT.h"
+//#include "../ECUAL/KEYPAD_DRIVER/KEYPAD.h"
+#include "../ECUAL/LCD_DRIVER/LCD.h"
+#include "../MCAL/ADC_DRIVER/ADC.h"
+
+int main(void) {
+	// Initializes the screen
+	LCD_Init();
+	uint16 RESULT_VALUE;
+
+	// Initializes the ADC Module
+	ADC_Init();
+
+	// Displays messages
+	LCD_displayString("ADC Value = ");
+
+	while (1) {
+		RESULT_VALUE = ADC_readChannel(0);
+		LCD_moveCursor(0, 12);
+
+		if (RESULT_VALUE >= 1000) {
+			LCD_intgerToString(RESULT_VALUE);
+		}
+		else {
+			LCD_intgerToString(RESULT_VALUE);
+			LCD_displayCharacter(' ');
+		}
+	}
+}
---
+++ ICU_EX1_HOSA/MCAL/ADC_DRIVER/ADC.c
@@ -0,0 +1,70 @@
+ /******************************************************************************
+ *
+ * Module: ADC (Analog to Digital Converter)
+ *
+ * File Name: ADC.c
+ *
+ * Description: Source file for the AVR ADC Driver
+ *
+ * Author: Hossam Mahmoud
+ *
+ *******************************************************************************/
+
+#include "ADC.h"
+
+#include "avr/io.h" /* To use the IO Ports Registers */
+#include <util/delay.h>
+
+
+// Initializes and enables the ADC Module to start functionality
+void ADC_Init(void) {
+
+	/* ADMUX Register Bits Description:
+	 * 7:6, REFS1:0 = 01 to choose AVCC = 5v as reference voltage
+	 * 5, ADLAR   = 0 right adjusted
+	 * 4:0, MUX4:0  = 00000 to choose channel 0 as initialization
+	 */
+	// Sets REFS1:0 to '01'
+	ADMUX = (1 << REFS0);
+
+	/* ADCSRA Register Bits Description:
+	 * ADEN = 1 Enable ADC
+	 * ADIE = 0 Disable ADC Interrupt
+	 * ADATE = 0 Disable Auto Trigger
+	 * ADPS2:0 = 111 to choose ADC_Clock = F_CPU/128 = 16Mhz/128 = 125Khz --> ADC must operate in range 50-200Khz
+	 */
+	// Enables ADC
+	ADCSRA = (1 << ADEN);
+
+	// Choose Prescaler: 128
+	ADCSRA = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+}
+
+
+// Reads the content written to the selected channel of the ADC
+uint16 ADC_readChannel(uint8 channel_num) {
+
+	// Insert Channel Number in ADMUX Procedure
+
+	// Clears first 5 bits to zero
+	ADMUX &= 0xE0; // 0b1110 0000
+
+	// Mask the Channel No. Input with max channel No.
+	channel_num &= 0x07; // 0b0000 0111
+
+	// Insert Channel no. into ADMUX Register
+	ADMUX |= channel_num;
+
+	// Starts ADC Conversion
+	ADCSRA = (1 << ADSC);
+
+	// Busy wait (Polling) untill ADIF = 1
+	while ( BIT_IS_CLR(ADCSRA, ADIF) );
+
+	// sET FLAG BY 1 TO CLEAR
+	ADCSRA = (1 << ADIF);
+
+	// Read Data
+	return ADC;
+}
+
